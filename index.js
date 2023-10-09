6e2a4bdb096fc4325d927afb18c3e21649ac0a19
DatNguyen <nguyentiendat39000@gmail.com>
2023-10-09 09:12:15 +0700

    connect-db

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
+const db = require("./src/config/db");
 const app = express();
 const port = 3002;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+db.connect();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
