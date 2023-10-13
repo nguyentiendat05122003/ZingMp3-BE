2425cefe4a7b1b6feddeaadf50869a1e6b7fc0e0
DatNguyen <nguyentiendat39000@gmail.com>
2023-10-13 14:55:36 +0700

    create typeSong model

---
+++ src/app/models/TypeSong.js
@@ -0,0 +1,28 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const { sequelize } = require("../../config/db");
+const TypeSong = sequelize.define(
+  "TypeSongs",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { timestamps: true }
+);
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Mô hình đã được đồng bộ hóa với cơ sở dữ liệu.");
+  })
+  .catch((error) => {
+    console.error("Lỗi khi đồng bộ hóa mô hình:", error);
+  });
+
+module.exports = TypeSong;
