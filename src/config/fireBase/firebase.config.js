8735db9bbd8fb810db016a600abd8297cc2283fd
DatNguyen <nguyentiendat39000@gmail.com>
2023-10-14 20:46:29 +0700

    create variable env

--- src/config/fireBase/firebase.config.js
+++ src/config/fireBase/firebase.config.js
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCodPrG_MSOQJjIeCdQLA_-xXhLOP9Yx9E",
-  authDomain: "zing-mp3-97a7c.firebaseapp.com",
-  projectId: "zing-mp3-97a7c",
-  storageBucket: "zing-mp3-97a7c.appspot.com",
-  messagingSenderId: "279354964974",
-  appId: "1:279354964974:web:674cdf83a768bc568eacff",
-  measurementId: "G-FVFJYXYJDT",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
 };
 
 module.exports = { firebaseConfig };
