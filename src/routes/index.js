5b543b21ece9a546b19ce530afc4a0cbd5d40b2a
DatNguyen <nguyentiendat39000@gmail.com>
2023-10-14 15:16:05 +0700

    controller playList

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const newRouter = require("./auth");
 const songRouter = require("./song");
-
+const playListRouter = require("./playList");
 function router(app) {
   app.use("/auth", newRouter);
   app.use("/song", songRouter);
+  app.use("/playList", playListRouter);
 }
 module.exports = router;
---
+++ src/routes/playList.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const playListController = require("../app/controllers/playListController");
+router.get("/", playListController.index);
+router.post("/add", playListController.add);
+router.put("/:id/edit", playListController.edit);
+router.delete("/:id/delete", playListController.delete);
+// router.post("/add", authController.addUser);
+module.exports = router;
